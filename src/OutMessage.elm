a060398788a8ba3b30c9b4b1e9ee50c1175b0a15
Folkert de Vries <flokkievids@gmail.com>
2017-06-27 21:30:04 +0200

    fix doc comment

--- src/OutMessage.elm
+++ src/OutMessage.elm
@@ -183,8 +183,8 @@ mapOutMsg f ( x, y, outMsg ) =
 
 {-| Add an outmessage to the normal type that `update` has.  Handy to use in a pipe:
 
-  ( { model | a = 1 }, Cmd.none )
-    |> addOutMsg Nothing
+    ( { model | a = 1 }, Cmd.none )
+        |> addOutMsg Nothing
 -}
 addOutMsg : outMsg -> ( model, Cmd msg ) -> ( model, Cmd msg, outMsg )
 addOutMsg outMsg ( model, cmd ) =
